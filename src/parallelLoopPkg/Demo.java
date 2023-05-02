f65265ef02089611851054854796d577a113219c
Maria Lebedeva <maria.lebedeva@jetbrains.com>
2023-05-02 18:44:25 +0200

    change instance access on static access

--- src/parallelLoopPkg/Demo.java
+++ src/parallelLoopPkg/Demo.java
@@ -10,7 +10,7 @@ public class Demo {
 
     private static void run() {
         int[] ints = {1,2,3,4,5};
-        new Calculator().calculate(ints);
+        Calculator.calculate(ints);
     }
 
 }
